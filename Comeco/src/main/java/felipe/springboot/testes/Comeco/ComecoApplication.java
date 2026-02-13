af3307d1c9bd7d0486cdadbf912f1bb6d114d2a8
StrngL <felipealvespedro@gmail.com>
2026-02-13 15:01:18 -0300

    Add files via upload

---
+++ Comeco/src/main/java/felipe/springboot/testes/Comeco/ComecoApplication.java
@@ -0,0 +1,13 @@
+package felipe.springboot.testes.Comeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComecoApplication.class, args);
+	}
+
+}
---
+++ Comeco/src/main/java/model/Tarefa.java
@@ -0,0 +1,50 @@
+package model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tarefas")
+
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    private boolean concluida;
+
+    public Tarefa() {
+
+    }
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+}
